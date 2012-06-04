7acb5e7435eb9fb9897b8dbfd0d82557d475638b
Hendrik Mans <hendrik@mans.de>
2012-06-04 12:45:55 +0200

    Only require ActiveSupport where it's needed

--- lib/happy-helpers.rb
+++ lib/happy-helpers.rb
@@ -1,6 +1,3 @@
-require 'active_support/inflector'
-require 'active_support/core_ext'
-
 require "happy-helpers/version"
 require 'happy-helpers/helpers'
 require 'happy-helpers/templates'
--- lib/happy-helpers/helpers/html.rb
+++ lib/happy-helpers/helpers/html.rb
@@ -1,3 +1,5 @@
+require 'active_support/core_ext'
+
 module HappyHelpers
   module Helpers
     module Html
